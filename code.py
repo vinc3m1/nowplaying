b4283bf935816d094b70fcebbe01b25e1a08fa64
Vince Mi <vinc3m1@gmail.com>
2011-11-17 14:46:42 -0800

    got templates working and all

--- code.py
+++ code.py
@@ -34,25 +34,10 @@ class nowplaying:
     def GET(self):
         
         iTunes = SBApplication.applicationWithBundleIdentifier_("com.apple.iTunes")
-        
-        if iTunes.isRunning() and iTunes.playerState() == 1800426320:
-            track = iTunes.currentTrack()
-
-            # print [i for i in dir(iTunes) if "_" not in i]
-
-            # if iTunes.currentPlaylist().name() == "Music" or len(iTunes.currentPlaylist().tracks()) > 200:
-            #     print "<h2>Current Track: %s - %s</h2>" % (track.artist(), track.name())
-            # else:
-            #     print "<ul><strong>Current Playlist: %s</strong>" % iTunes.currentPlaylist().name()
-            #     for tr in iTunes.currentPlaylist().tracks():
-            #         if tr.name() == track.name() and tr.artist() == track.artist():
-            #             print "<li style='color:blue'>%s - %s</li>" % (tr.artist(), tr.name())
-            #         else:
-            #             print "<li>%s - %s</li>" % (tr.artist(), tr.name())
-            # print "</ul>"
+        track = iTunes.currentTrack()
         
         web.header('Content-Type','text/html; charset=utf-8') 
-        return render.nowplaying()
+        return render.nowplaying(track=track, iTunes=iTunes, len=len)
 
 if __name__ == "__main__":
     app.run()
